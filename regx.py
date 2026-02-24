429e6ed0cfdd5f60d0393ec6d792379b6d47bc57
mukataev555 <mukataevalibek555@gmail.com>
2026-02-24 10:31:30 +0500

    pr5

--- regx.py
+++ regx.py
@@ -9,3 +9,110 @@ if x:
   print("YES! We have a match!")
 else:
   print("No match")
+
+
+txt = "The rain in Spain"
+
+#Find all lower case characters alphabetically between "a" and "m":
+
+x = re.findall("[a-e]", txt)
+print(x)
+
+import re
+
+txt = "That will be 59 dollars"
+
+#Find all digit characters:
+
+x = re.findall("\d", txt)
+print(x)
+
+import re
+
+txt = "hello planet"
+
+#Search for a sequence that starts with "he", followed by two (any) characters, and an "o":
+
+x = re.findall("he..o", txt)
+print(x)
+
+import re
+
+txt = "hello planet"
+
+#Check if the string starts with 'hello':
+
+x = re.findall("^hello", txt)
+if x:
+  print("Yes, the string starts with 'hello'")
+else:
+  print("No match")
+
+import re
+
+txt = "hello planet"
+
+#Check if the string ends with 'planet':
+
+x = re.findall("planet$", txt)
+if x:
+  print("Yes, the string ends with 'planet'")
+else:
+  print("No match")
+
+import re
+
+txt = "hello planet"
+
+#Search for a sequence that starts with "he", followed by 0 or more  (any) characters, and an "o":
+
+x = re.findall("he.*o", txt)
+
+print(x)
+
+import re
+
+txt = "hello planet"
+
+#Search for a sequence that starts with "he", followed by 1 or more  (any) characters, and an "o":
+
+x = re.findall("he.+o", txt)
+
+print(x)
+
+import re
+
+txt = "hello planet"
+
+#Search for a sequence that starts with "he", followed by 0 or 1  (any) character, and an "o":
+
+x = re.findall("he.?o", txt)
+
+print(x)
+
+#This time we got no match, because there were not zero, not one, but two characters between "he" and the "o"
+
+import re
+
+txt = "hello planet"
+
+#Search for a sequence that starts with "he", followed excactly 2 (any) characters, and an "o":
+
+x = re.findall("he.{2}o", txt)
+
+print(x)
+
+import re
+
+txt = "The rain in Spain falls mainly in the plain!"
+
+#Check if the string contains either "falls" or "stays":
+
+x = re.findall("falls|stays", txt)
+
+print(x)
+
+if x:
+  print("Yes, there is at least one match!")
+else:
+  print("No match")
