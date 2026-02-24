1162a98622b3229be8a0e584b96243abc8ea2136
mukataev555 <mukataevalibek555@gmail.com>
2026-02-24 10:37:37 +0500

    pr5

---
+++ sets.py
@@ -0,0 +1,125 @@
+import re
+
+txt = "The rain in Spain"
+
+#Check if the string has any a, r, or n characters:
+
+x = re.findall("[arn]", txt)
+
+print(x)
+
+if x:
+  print("Yes, there is at least one match!")
+else:
+  print("No match")
+
+
+import re
+
+txt = "The rain in Spain"
+
+#Check if the string has any characters between a and n:
+
+x = re.findall("[a-n]", txt)
+
+print(x)
+
+if x:
+  print("Yes, there is at least one match!")
+else:
+  print("No match")
+
+import re
+
+txt = "The rain in Spain"
+
+#Check if the string has other characters than a, r, or n:
+
+x = re.findall("[^arn]", txt)
+
+print(x)
+
+if x:
+  print("Yes, there is at least one match!")
+else:
+  print("No match")
+
+
+import re
+
+txt = "The rain in Spain"
+
+#Check if the string has any 0, 1, 2, or 3 digits:
+
+x = re.findall("[0123]", txt)
+
+print(x)
+
+if x:
+  print("Yes, there is at least one match!")
+else:
+  print("No match")
+
+
+import re
+
+txt = "8 times before 11:45 AM"
+
+#Check if the string has any digits:
+
+x = re.findall("[0-9]", txt)
+
+print(x)
+
+if x:
+  print("Yes, there is at least one match!")
+else:
+  print("No match")
+
+
+import re
+
+txt = "8 times before 11:45 AM"
+
+#Check if the string has any two-digit numbers, from 00 to 59:
+
+x = re.findall("[0-5][0-9]", txt)
+
+print(x)
+
+if x:
+  print("Yes, there is at least one match!")
+else:
+  print("No match")
+
+
+import re
+
+txt = "8 times before 11:45 AM"
+
+#Check if the string has any characters from a to z lower case, and A to Z upper case:
+
+x = re.findall("[a-zA-Z]", txt)
+
+print(x)
+
+if x:
+  print("Yes, there is at least one match!")
+else:
+  print("No match")
+
+import re
+
+txt = "8 times before 11:45 AM"
+
+#Check if the string has any + characters:
+
+x = re.findall("[+]", txt)
+
+print(x)
+
+if x:
+  print("Yes, there is at least one match!")
+else:
+  print("No match")
+
